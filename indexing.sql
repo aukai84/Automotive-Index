0ed16f91115979df62840573d3768215566f417b
Aukai Tirrell <mtirrell@hawaii.edu>
2017-01-19 19:06:15 -1000

    select all years between 2010 and 2015

--- indexing.sql
+++ indexing.sql
@@ -18,3 +18,4 @@ CREATE DATABASE "indexed_cars" WITH OWNER "indexed_cars_user";
 SELECT DISTINCT "make_title" FROM "car_models" WHERE "make_code" = 'LAM';
 SELECT DISTINCT "model_title" FROM "car_models" WHERE "make_code" = 'NISSAN' AND "model_code" = 'GT-R';
 SELECT "make_code", "model_code", "model_title", "year" FROM "car_models" WHERE "make_code" = 'LAM';
+SELECT * FROM "car_models" WHERE "year" <= 2015 AND "year" >= 2010;
