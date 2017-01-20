781f8edec9d208f3fb6ad549cd8bbef498c4051e
Aukai Tirrell <mtirrell@hawaii.edu>
2017-01-19 18:37:31 -1000

    kinda working?

--- indexing.sql
+++ indexing.sql
@@ -1,5 +1,8 @@
 \c matthewtirrell
-DROP USER "indexed_cars_user";
+DROP USER IF EXISTS "indexed_cars_user";
 CREATE USER "indexed_cars_user";
-CREATE DATABASE "indexed_cars" WITH OWNER "indexed_cars_user";
 DROP DATABASE IF EXISTS "indexed_cars";
+CREATE DATABASE "indexed_cars" WITH OWNER "indexed_cars_user";
+\c "indexed_cars";
+\i scripts/car_models.sql
+
