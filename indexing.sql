6bd6d928475c5a75818b7f6925bd9caab0a5fc13
Aukai Tirrell <mtirrell@hawaii.edu>
2017-01-19 18:56:00 -1000

    selected make model title year from models with LAM CODE..1360 rows

--- indexing.sql
+++ indexing.sql
@@ -16,3 +16,5 @@ CREATE DATABASE "indexed_cars" WITH OWNER "indexed_cars_user";
 \i scripts/car_model_data.sql;
 \i scripts/car_model_data.sql;
 SELECT DISTINCT "make_title" FROM "car_models" WHERE "make_code" = 'LAM';
+SELECT DISTINCT "model_title" FROM "car_models" WHERE "make_code" = 'NISSAN' AND "model_code" = 'GT-R';
+SELECT "make_code", "model_code", "model_title", "year" FROM "car_models" WHERE "make_code" = 'LAM';
