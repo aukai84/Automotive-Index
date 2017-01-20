1179a04572481ef925c18c1c750614c7dcc46f5c
Aukai Tirrell <mtirrell@hawaii.edu>
2017-01-19 19:13:25 -1000

    noted time for queries

--- indexing.sql
+++ indexing.sql
@@ -15,8 +15,8 @@ CREATE DATABASE "indexed_cars" WITH OWNER "indexed_cars_user";
 \i scripts/car_model_data.sql;
 \i scripts/car_model_data.sql;
 \i scripts/car_model_data.sql;
-SELECT DISTINCT "make_title" FROM "car_models" WHERE "make_code" = 'LAM';
-SELECT DISTINCT "model_title" FROM "car_models" WHERE "make_code" = 'NISSAN' AND "model_code" = 'GT-R';
-SELECT "make_code", "model_code", "model_title", "year" FROM "car_models" WHERE "make_code" = 'LAM';
-SELECT * FROM "car_models" WHERE "year" <= 2015 AND "year" >= 2010;
-SELECT * FROM "car_models" WHERE "year" = 2010;
+SELECT DISTINCT "make_title" FROM "car_models" WHERE "make_code" = 'LAM';--Time: 32.489 ms
+SELECT DISTINCT "model_title" FROM "car_models" WHERE "make_code" = 'NISSAN' AND "model_code" = 'GT-R';--Time: 31.160 ms
+SELECT "make_code", "model_code", "model_title", "year" FROM "car_models" WHERE "make_code" = 'LAM';--Time: 26.285 ms
+SELECT * FROM "car_models" WHERE "year" <= 2015 AND "year" >= 2010;--Time: 102.636 ms
+SELECT * FROM "car_models" WHERE "year" = 2010;--Time: 45.922 ms
