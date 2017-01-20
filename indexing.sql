05d88252817d47ce0e17f1b52474f84212fd9168
Aukai Tirrell <mtirrell@hawaii.edu>
2017-01-19 18:43:27 -1000

    added data to car_models table 10 times.. with 223389 rows..

--- indexing.sql
+++ indexing.sql
@@ -3,6 +3,15 @@ DROP USER IF EXISTS "indexed_cars_user";
 CREATE USER "indexed_cars_user";
 DROP DATABASE IF EXISTS "indexed_cars";
 CREATE DATABASE "indexed_cars" WITH OWNER "indexed_cars_user";
-\c "indexed_cars";
+\c indexed_cars
 \i scripts/car_models.sql;
-
+\i scripts/car_model_data.sql;
+\i scripts/car_model_data.sql;
+\i scripts/car_model_data.sql;
+\i scripts/car_model_data.sql;
+\i scripts/car_model_data.sql;
+\i scripts/car_model_data.sql;
+\i scripts/car_model_data.sql;
+\i scripts/car_model_data.sql;
+\i scripts/car_model_data.sql;
+\i scripts/car_model_data.sql;
